41faffc60b4a221e4ab0d43a8a7d6dc126cb1083
Yassine0727 <123115686+Yassine0727@users.noreply.github.com>
2023-04-30 20:27:01 +0200

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,55 @@
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:probot/config.dart';
+
+class Home extends StatelessWidget {
+  final homeCtrl = Get.put(HomeController());
+
+  Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomeController>(builder: (_) {
+      return CommonStream(
+        child: Scaffold(
+          key: homeCtrl.scaffoldKey,
+          drawer: const CommonDrawer(),
+          body: Stack(alignment: Alignment.bottomCenter, children: [
+            Column(
+              children: [
+                HomeTopLayout(onTap: () => homeCtrl.scaffoldKey.currentState!
+                    .openDrawer(),),
+                const DottedLines(),
+                Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const VSpace(Sizes.s20),
+                      ...homeCtrl.homeOptionList.asMap().entries.map((e) {
+                        return ((appCtrl.firebaseConfigModel!.isChatShow! &&
+                            e.key == 0) ||
+                            (appCtrl.firebaseConfigModel!
+                                .isImageGeneratorShow! &&
+                                e.key == 1) ||
+                            ((appCtrl.firebaseConfigModel!
+                                .isTextCompletionShow! &&
+                                e.key == 2)))
+                            ? OptionCard(homeOptionModel: e.value).inkWell(
+                            onTap: () => homeCtrl.onOptionTap(e.value))
+                            : Container();
+                      }).toList(),
+                    ]).marginSymmetric(horizontal: Sizes.s5)
+              ],
+            ),
+            if (appCtrl.firebaseConfigModel!.isAddShow!)
+              if (homeCtrl.bannerAd != null)
+                AdWidget(ad: homeCtrl.bannerAd!)
+                    .height(Sizes.s5)
+                    .paddingOnly(bottom: Insets.i1)
+                    .width(MediaQuery.of(context).size.width)
+          ]),
+          backgroundColor: appCtrl.appTheme.bg1,
+        ),
+      );
+    });
+  }
+}
